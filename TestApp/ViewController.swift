9fec434f226c95296f2e1826785c273a86ca82cd
junhyuk <dejavukk1@gmail.com>
2020-03-25 16:45:44 +0900

    테스트 연동

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         print("ggg")
+        print("gggggggg")
+        print("깃허브 연동중.")
     }
 
 
